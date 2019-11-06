245d3553297cc5ed9b49ad051898ab9dc2435f7a
Anne Poso <anneposo1@csu.fullerton.edu>
2019-11-05 20:12:30 -0800

    added terminal, nonterminal cases

--- parser.c
+++ parser.c
@@ -3,6 +3,25 @@
 #include <stdbool.h>
 #include <string.h>
 
+bool isTerminal(char ch) {
+	const char* terminals[] = { "if", "else", "then", "endif", "while", "whileend", "do", "doend", "for",
+                              "forend", "input", "output", "and", "or" , "function", ";" };
+	int match = 0;
+
+	for (int i = 0; i < 13; i++) {
+		if (strchr(terminals[i], ch)) {
+			match = 1;
+			break;
+		}
+	}
+	return match;
+}
+
+bool isNonTerminal(char ch) {
+
+}
+
+
 int main () {
 
 
