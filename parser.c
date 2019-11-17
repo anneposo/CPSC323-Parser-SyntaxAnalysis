f9c1d04f75106a9442ea6cacdf7fa51891ed26f0
Anne Poso <anneposo1@csu.fullerton.edu>
2019-11-16 22:29:10 -0800

    Added command line args for source code file

--- parser.c
+++ parser.c
@@ -3,14 +3,11 @@
 #include <stdbool.h>
 #include <string.h>
 
-void getFile();
-void openInputFile();
-void openOutputFile();
+void openFiles(char*);
 void closeFiles();
 
-char sourceCodeFile[BUFSIZE]; //holds filename of source code to parse
-FILE* fp; // file pointer to input source code file
-FILE* outputPtr; // file pointer to output text file
+FILE *fp; // file pointer to input source code file
+FILE *outputPtr; // file pointer to output text file
 
 enum NonTerminalSymbols {
 	NT_S, // Statement
@@ -29,23 +26,14 @@ enum TerminalSymbols {
 	T_ID				// identifier
 };
 
-void getFile(void) {		// getFile gets filename of source code to parse
-	printf("Enter source code filename: ");
-	fgets(sourceCodeFile, sizeof(sourceCodeFile), stdin);
-  sourceCodeFile[strcspn(sourceCodeFile, "\n")] = '\0'; // removes newline from user input
-}
-
-void openInputFile(void) {
-	getFile();
-	fp = fopen(sourceCodeFile, "r");
+void openFiles(char *fileName) {
+	fp = fopen(fileName, "r");
 	if (fp == NULL) {
 		printf("Could not open file.\n");
 		exit(1);
-	} else { printf("Opened %s successfully.\n\n", sourceCodeFile); }
-}
+	} else { printf("Opened %s successfully.\n\n", fileName); }
 
-void openOutputFile(void) {
-  outputPtr = fopen("output.txt", "w"); // open output file with write permissions
+	outputPtr = fopen("output.txt", "w"); // open output file with write permissions
 }
 
 void closeFiles(void) { // closes both input and output files
@@ -53,27 +41,10 @@ void closeFiles(void) { // closes both input and output files
 	fclose(outputPtr);
 }
 
-bool isTerminal(char ch) {
-	const char* terminals[] = { "if", "else", "then", "endif", "while", "whileend", "do", "doend", "for",
-                              "forend", "input", "output", "and", "or" , "function", ";" };
-	int match = 0;
 
-	for (int i = 0; i < 13; i++) {
-		if (strchr(terminals[i], ch)) {
-			match = 1;
-			break;
-		}
-	}
-	return match;
-}
+int main (int argc, char *argv[]) {
+	openFiles(argv[1]);
 
-bool isNonTerminal(char ch) {
-
-}
-
-
-int main () {
-
-
+	closeFiles();
   return 0;
 }
