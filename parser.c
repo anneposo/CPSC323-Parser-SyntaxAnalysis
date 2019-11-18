d2ecb035392a7a6f99471c33b174eb90fe062fa9
Anne Poso <anneposo1@csu.fullerton.edu>
2019-11-17 23:05:52 -0800

    Implemented production rule functions for first iteration

--- parser.c
+++ parser.c
@@ -2,25 +2,76 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 #include "parser.h"
 
-enum NonTerminalSymbols {
-	NT_S, // Statement
-	NT_E, // Expression
-	NT_A, // Assignment
-	NT_T, // Term
-	NT_F  // Factor
+enum Symbols {
+	NT_S,       // Statement
+	NT_E,       // Expression
+	NT_A,       // Assignment
+	NT_T,       // Term
+	NT_F,       // Factor
+	T_PLUS,     // +
+	T_MINUS,    // -
+	T_MULTI,    // *
+	T_DIV,      // /
+	T_L_PARENS, // (
+	T_R_PARENS, // )
+	T_ID,       // identifier
+	T_NUM,      // integer number
+	T_EMPTY     // empty (epsilon)
 };
-enum TerminalSymbols {
-	T_PLUS,			// +
-	T_MINUS,		// -
-	T_MULTI,		// *
-	T_DIV,			// /
-	T_L_PARENS,	// (
-	T_R_PARENS,	// )
-	T_ID				// identifier
-	T_EMPTY			// empty (epsilon)
-};
+
+enum Symbols lexer(char ch) {
+	switch(ch) {
+		case '+':
+			printf("\nToken: Operator\tLexeme: %c\n", ch);
+			return T_PLUS;
+			break;
+		case '-':
+			printf("\nToken: Operator\tLexeme: %c\n", ch);
+			return T_MINUS;
+			break;
+		case '*':
+			printf("\nToken: Operator\tLexeme: %c\n", ch);
+			return T_MULTI;
+			break;
+		case '/':
+			printf("\nToken: Operator\tLexeme: %c\n", ch);
+			return T_DIV;
+			break;
+		case '(':
+			printf("\nToken: Separator\tLexeme: %c\n", ch);
+			return T_L_PARENS;
+			break;
+		case ')':
+			printf("\nToken: Separator\tLexeme: %c\n", ch);
+			return T_R_PARENS;
+			break;
+		case '=':
+			printf("\nToken: Operator\tLexeme: %c\n", ch);
+			break;
+		case ';':
+			printf("\nToken: Separator\tLexeme: %c\n", ch);
+			printf("e -> epsilon\n");
+			return T_EMPTY;
+			break;
+		default:
+			if (isalpha(ch) > 0) {
+				printf("\nToken: Identifier\tLexeme: %c\n", ch);
+				return T_ID;
+			}
+			else if (isdigit(ch) > 0) {
+				printf("\nToken: Identifier\tLexeme: %c\n", ch);
+				return T_NUM;
+			}
+			else {
+				printf("e -> epsilon\n");
+				return T_EMPTY;
+			}
+			break;
+	}
+}
 
 void openFiles(char *fileName) {
 	fp = fopen(fileName, "r");
@@ -48,10 +99,11 @@ T' -> *FT' | /FT' | e     TermPrime       ->  * Factor TermPrime | / Factor Term
 F -> ( E ) | id           Factor          ->  ( Expression ) | id
 
 ********************************************************************************************************/
-bool isFollowTP(const char *ch) { // Follow(T') = { +, ), $ }
+bool isFollowTP(char ch) { // Follow(T') = { +, ), $ }
 	const char *followTP[] = { "+", ")", "\0", ";" };
+	int match = 0;
 	for (int i = 0; i < 4; i++) {
-		if (strcmp(followTP[i], ch) == 0) {
+		if (strchr(followTP[i], ch) == 0) {
 			match = 1;
 			break;
 		}
@@ -61,6 +113,7 @@ bool isFollowTP(const char *ch) { // Follow(T') = { +, ), $ }
 
 bool isFollowEP(const char *ch) { // Follow(E') = { ), $ }
 	const char *followEP[] = { ")", "\0", ";" };
+	int match = 0;
 	for (int i = 0; i < 3; i++) {
 		if (strcmp(followEP[i], ch) == 0) {
 			match = 1;
@@ -71,58 +124,191 @@ bool isFollowEP(const char *ch) { // Follow(E') = { ), $ }
 }
 
 void match(char ch) {
-	if (ch == nextChar) { nextChar = buffer[index++]; } // if matches, go to next
+	if (ch == nextChar) { next(); } // if matches, go to next
 	else {
 		printf("Error matching rule \n\n");
 		exit(2);
 	}
 }
 
+void next() {
+	static int i = 0;
+	nextChar = buffer[i++];
+}
+
 // F -> ( E ) | id
 void F() {
 	if (nextChar == '(') {
+		nextChar = lexer(nextChar);
 		match('(');
-		//E(); 								// need to implement E procedure
+		E(); 								// need to implement E procedure
 		match(')');
-		// print production rule F
+		printf("F -> ( E ) | id\n");
 	}
-	else if(nextChar == T_ID) {
+	else if(isalpha(nextChar) > 0) {
+		nextChar = lexer(nextChar);
 		match(T_ID);
-		// print production rule F
+		printf("F -> ( E ) | id\n");
 	}
-	else { printf("Syntax Error. Token in First of F expected."); }
+	else {
+		printf("Syntax Error. Expected token in First F.");
+		errorF = 1;
+	}
 }
 
 // T' -> *FT' | /FT' | e
 void TP() { //TPrime
 	if (nextChar == '*') {
-		match('*');
+		nextChar = lexer(nextChar);
+		printf("T' -> *FT' | /FT' | e\n");
+		match(T_MULTI);
 		F();
 		TP();
-		// print production rule T'
 	}
 	else if (nextChar == '/') {
-		match('/');
+		nextChar = lexer(nextChar);
+		printf("T' -> *FT' | /FT' | e\n");
+		match(T_DIV);
 		F();
 		TP();
-		// print production rule T'
 	}
-	else if (!(isFollowTP(nextChar))) {
+	else if(nextChar == ';' || nextChar == '\0') {
+		nextChar = lexer(nextChar);
+		printf("T' -> *FT' | /FT' | e\n");
 		match(T_EMPTY);
-		// print production rule T'
 	}
-	else { printf("Syntax Error. Token in First of T' expected."); }
+	else {
+		printf("Syntax Error. Expected token in First T'.\n");
+		errorTP = 1;
+	}
 }
 
+//T -> FT'
+void T() {
+	if(nextChar == '(') {
+		nextChar = lexer(nextChar);
+		printf("T -> FT'\n");
+		match(T_L_PARENS);
+		TP();
+	}
+	else if(isalpha(nextChar) > 0) {
+		nextChar = lexer(nextChar);
+		printf("T -> FT'\n");
+		match(T_ID);
+		TP();
+	}
+	else {
+		printf("Syntax Error. Expected token in First T.\n");
+		errorT = 1;
+	}
+}
+
+//E' -> +TE' | -TE' | e
+void EP() {
+	if(nextChar == '+') {
+		nextChar = lexer(nextChar);
+		printf("E' -> +TE' | -TE' | e\n");
+		match(T_PLUS);
+		T();
+		EP();
+	}
+	else if(nextChar == '-') {
+		nextChar = lexer(nextChar);
+		printf("E' -> +TE' | -TE' | e\n");
+		match(T_MINUS);
+		T();
+		EP();
+	}
+	else if(nextChar == ';' || nextChar == '\0') {
+		nextChar = lexer(nextChar);
+		printf("E' -> +TE' | -TE' | e\n");
+		match(T_EMPTY);
+	}
+	else {
+		printf("Syntax Error. Expected token in First E'.\n");
+		errorEP = 1;
+	}
+}
+
+//E -> TE'
+void E() {
+	if(nextChar == '(') {
+		nextChar = lexer(nextChar);
+		printf("E -> TE'\n");
+		match(T_L_PARENS);
+		EP();
+	}
+	else if(isalpha(nextChar) > 0) {
+		nextChar = lexer(nextChar);
+		printf("E -> TE'\n");
+		match(T_ID);
+		EP();
+	}
+	else {
+		printf("Syntax Error. Expected token in First E.\n");
+		errorE = 1;
+	}
+}
+
+//A -> id = E;
+void A() {
+	if(isalpha(nextChar) > 0) {
+		printf("A -> id = E\n");
+		next();
+		if (nextChar == '=') {
+			lexer(nextChar);
+			match('=');
+			E();
+		}
+		else {
+			printf("Syntax Error. Mismatch in Rule A.\n");
+			errorA = 1;
+		}
+	}
+	else {
+		printf("Syntax Error. Expected token in First A.\n");
+		errorA = 1;
+	}
+}
+
+//S -> A
+void S() {
+	if(isalpha(nextChar) > 0) {
+		nextChar = lexer(nextChar);
+		printf("S -> A\n");
+		match(T_ID);
+		A();
+	}
+	else {
+		printf("Syntax Error. Expected token in First S.\n");
+		errorS = 1;
+	}
+}
+
+void parser() {
+	S();
+}
 
 int main (int argc, char *argv[]) {
 	char ch;
-	openFiles(argv[1]);
+	printf("Enter a string: ");
+	scanf("%s", buffer);
+	nextChar = buffer[0];
+	//openFiles(argv[1]);
 
-	while ((ch = fgetc(fp)) != EOF) {
-    printf("%c", ch);
-  }
+	// for (int i = 0; i < sizeof(buffer); i++) {
+	// 	lexer(buffer[i]);
+	// }
+	parser();
+	//while ((ch = fgetc(fp)) != EOF) {
+  // printf("%c", ch);
+  //}
 
-	closeFiles();
+
+	if (nextChar == '\0') {
+		printf("\nParsing completed successfully.\n");
+	}
+
+	//closeFiles();
   return 0;
 }
--- parser.h
+++ parser.h
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFSIZE 100
+
+enum Symbols lexer(char);
 void openFiles(char*);
 void closeFiles();
+bool isFollowTP(char ch);
+bool isFollowEP(const char *ch);
+void match(char ch);
+void next();
+void F();
+void TP();
+void T();
+void EP();
+void E();
+void A();
+void S();
+void parser();
 
 FILE *fp; // file pointer to input source code file
 FILE *outputPtr; // file pointer to output text file
+
+char nextChar;
+char buffer[BUFSIZE];
+
+int errorS  = 0;
+int errorA  = 0;
+int errorE  = 0;
+int errorEP = 0;
+int errorT  = 0;
+int errorTP = 0;
+int errorF  = 0;
